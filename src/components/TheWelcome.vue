93a02659b40614954c612eaf9c56555bef48f5ae
developer71@azet.sk <developer71@azet.sk>
2023-11-24 17:05:31 +0100

    Secound commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,32 @@
+
+<script setup lang="ts">
+import {ref, reactive} from "vue";
+
+const msgClass = ref("msg1")
+const msg = ref( "Hello world")
+const counter = reactive({count:0})
+const condition = ref(true)
+
+</script>
+<template>
+
+<h1 :class="msgClass">
+ 
+
+ {{msg}}
+ {{ counter.count }}
+ 
+
+</h1>
+<p v-if="condition">Teraz je true</p>
+<p v-else>Teraz je false</p>
+</template>
+
+<style>
+.msg1{
+  
+  background-color:yellow;
+
+
+  }
+</style>
